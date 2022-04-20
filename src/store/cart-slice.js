ff24d983e17ff12a44641a6b2095f7a131f698b2
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-20 01:56:47 +0000

    Implementing addItemToCart reducer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -9,7 +9,22 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: initialCartState,
   reducers: {
-    addItemToCart(state, action) {},
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.totalPrice,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
 
     removeItemFromCart(state, action) {},
   },
