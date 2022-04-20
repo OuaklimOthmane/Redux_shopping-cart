b2c1fe2872d2d793daf84168583be03a916c0fa3
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-20 02:12:31 +0000

    defining some dummy products for test

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,21 +1,34 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "My first book",
+    price: 5,
+    description: "My first book i ever wrote",
+  },
+  {
+    id: "p2",
+    title: "My second book",
+    price: 7,
+    description: "My second book i wrote",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test 1"
-          price={6}
-          description="This is a first product - amazing!"
-        />
-        <ProductItem
-          title="Test 2"
-          price={5}
-          description="This is a second product - amazing!"
-        />
+        {PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
