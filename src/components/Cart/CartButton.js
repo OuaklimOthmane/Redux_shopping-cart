14444640e8c7a5e57af7a18adc74b1858fc52303
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-20 01:34:40 +0000

    Implementing toggle cart using redux-toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
