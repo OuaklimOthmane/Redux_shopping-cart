c5b74db96a90889900bbc3e9160323ee44438236
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-21 17:22:37 +0000

    add key

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -23,6 +23,7 @@ const Products = (props) => {
       <ul>
         {PRODUCTS.map((product) => (
           <ProductItem
+            key={product.id}
             id={product.id}
             title={product.title}
             price={product.price}
