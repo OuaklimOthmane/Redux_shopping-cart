39e8a41c79a09b42a2b0582a3f4571e1ed23060c
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-21 17:23:49 +0000

    price fixed

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -12,12 +12,13 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
           price: newItem.price,
           quantity: 1,
-          totalPrice: newItem.totalPrice,
+          totalPrice: newItem.price,
           name: newItem.title,
         });
       } else {
@@ -29,6 +30,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
