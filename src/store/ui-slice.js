619871542c7442a3b5d1218ca7dab8b1166952df
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-25 00:11:19 +0000

    create a cart thunk

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,5 +1,6 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import { createSlice } from "@reduxjs/toolkit";
 
+//! Create ui slice :
 const uiSlice = createSlice({
   name: "ui",
   initialState: { cartIsVisible: false, notification: null },
@@ -20,4 +21,52 @@ const uiSlice = createSlice({
 
 export default uiSlice.reducer;
 
+//! Create actions :
 export const uiActions = uiSlice.actions;
+
+//! Create action creator thunk :
+export const sendCartData = function (cart) {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Pending ...",
+        message: "Sending cart request ...",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://redux-shopping-cart-4c8fe-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart request failed !!");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success !",
+          message: "Sending cart request successfully !",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error !",
+          message: "Sending cart request failed !!",
+        })
+      );
+    }
+  };
+};
