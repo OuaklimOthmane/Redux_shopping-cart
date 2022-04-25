91adcf252ea1b87077695b656423d5c5522d5188
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-25 00:58:49 +0000

    handle the undefind bug when the cart items is empty

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -47,7 +52,10 @@ export const sendCartData = function (cart) {
         "https://redux-shopping-cart-4c8fe-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
