8a65bb8e0416fcc4e36cd0df876a158a451f47bc
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-25 00:39:19 +0000

    imlementing replaceCart reducer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -9,6 +9,11 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: initialCartState,
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
